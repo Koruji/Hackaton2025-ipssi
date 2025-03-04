344e226f8860609d1eeacdf831d540723343831f
Koruji <jadecottin212@gmail.com>
2025-03-04 16:19:32 +0100

    Redirection ADMIN / USER après login + ajout lien Gestion chantier et ouvrier + mise en place du dashboard administrateur

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Chantier;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/dashboard', name: 'app_home_admin')]
+    public function index(EntityManagerInterface $em): Response
     {
+        $chantiers = $em->getRepository(Chantier::class)->findAll();
         return $this->render('home/index.html.twig', [
+            'chantiers' => $chantiers,
             'controller_name' => 'HomeController',
         ]);
     }
