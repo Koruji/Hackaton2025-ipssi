c85470eb66a54dea873a3716e4bd623837fa6b18
Koruji <jadecottin212@gmail.com>
2025-03-05 12:54:42 +0100

    Début de création de la page mission

--- src/Form/MissionType.php
+++ src/Form/MissionType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Chantier;
 use App\Entity\Employes;
 use App\Entity\Mission;
+use App\Repository\EmployesRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,19 +16,22 @@ class MissionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('employe', EntityType::class, [
+                'label' => 'Ouvrier',
+                'class' => Employes::class,
+                'choice_label' => function(Employes $employes) {
+                    return $employes->getNom() . ' ' . $employes->getPrenom();
+                },
+                'query_builder' => function(EmployesRepository $repoEmployes) {
+                    return $repoEmployes->findEmployesByRoleOuvrier();
+                }
+            ])
             ->add('dateDebut', null, [
+                'label' => '',
                 'widget' => 'single_text',
             ])
             ->add('dateFin', null, [
                 'widget' => 'single_text',
-            ])
-            ->add('chantier', EntityType::class, [
-                'class' => Chantier::class,
-                'choice_label' => 'id',
-            ])
-            ->add('employe', EntityType::class, [
-                'class' => Employes::class,
-                'choice_label' => 'id',
             ])
         ;
     }
