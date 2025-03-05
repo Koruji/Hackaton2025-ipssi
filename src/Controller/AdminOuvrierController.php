10704db3edab7913c21a8a22df54a279e5f77e87
Romain Liénard <rhum1@MacBook-Air-de-Romain.local>
2025-03-05 17:52:45 +0100

    style button admin ouvrier/chantier

--- src/Controller/AdminOuvrierController.php
+++ src/Controller/AdminOuvrierController.php
@@ -13,7 +13,7 @@ final class AdminOuvrierController extends AbstractController
     #[Route('/admin/ouvrier', name: 'app_admin_ouvrier')]
     public function index(EntityManagerInterface $em): Response
     {
-        $employes = $em->getRepository(Employes::class)->findAll();
+        $employes = $em->getRepository(Employes::class)->findEmployesByRoleOuvrier();
 
         return $this->render('admin_ouvrier/index.html.twig', [
             'employes' => $employes,
