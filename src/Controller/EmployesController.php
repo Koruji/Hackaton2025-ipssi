d43da9ea10127cd30ced25a1ad4916771290da94
Romain Liénard <rhum1@MacBook-Air-de-Romain.local>
2025-03-05 12:52:41 +0100

    fix admin

--- src/Controller/EmployesController.php
+++ src/Controller/EmployesController.php
@@ -34,32 +34,4 @@ final class EmployesController extends AbstractController
             'competences' => $competences,
         ]);
     }
-
-    // #[Route('/chantier/{id}/assigner', name: 'chantier_assign')]
-    // public function assigner(Chantier $chantier, EntityManagerInterface $em): Response
-    // {
-    //     $employes = $em->getRepository(Employes::class)->findAll();
-
-    //     return $this->render('chantier/assign.html.twig', [
-    //         'chantier' => $chantier,
-    //         'employes' => $employes
-    //     ]);
-    // }
-
-    #[Route('/chantier/{id}/assigner/save', name: 'chantier_assign_save', methods: ['POST'])]
-    public function assignerSave(Request $request, Chantier $chantier, EntityManagerInterface $em): Response
-    {
-        $employeIds = $request->request->get('employes', []);
-        $employes = $em->getRepository(Employes::class)->findBy(['id' => $employeIds]);
-
-        foreach ($employes as $employe) {
-            // Ici, tu peux ajouter une entité "Mission" si nécessaire
-            // Sinon, ajouter les employés directement au chantier
-            // Exemple : $chantier->addEmploye($employe);
-        }
-
-        $em->flush();
-
-        return $this->redirectToRoute('chantier_list');
-    }
 }
