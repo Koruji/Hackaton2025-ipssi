2dfcb07381679e8bed291e30c442887c30301715
Jade COTTIN <129370642+Koruji@users.noreply.github.com>
2025-03-06 09:37:46 +0100

    Update ChantierController.php

--- src/Controller/ChantierController.php
+++ src/Controller/ChantierController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Chantier;
 use App\Entity\Employes;
+use App\Entity\Mission;
 use App\Form\ChantierType;
 use App\Repository\ChantierRepository;
-use App\Repository\CompetenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,25 +16,16 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/chantier')]
 final class ChantierController extends AbstractController
 {
-    #[Route('/chantier/{id}', name: 'show_chantier')] //utilisé pour quand on clique sur un chantier dashboard
-    public function show(EntityManagerInterface $em, $id, ChantierRepository $chantierRepository): Response
+    #[Route('/show/{id}', name: 'show_chantier')] //utilisé pour quand on clique sur un chantier dashboard
+    public function show(EntityManagerInterface $em, $id): Response
     {
         $competences = $em->getRepository(Chantier::class)->findCompetencesByChantierId($id);
-
         $chantier = $em->getRepository(Chantier::class)->find($id);
-        $ouvriers = $em->getRepository(Employes::class)->findEmployesByChantierId($id);
+        $missions = $em->getRepository(Mission::class)->findMissionsByChantierId($id);
         return $this->render('chantier/show.html.twig', [
             'chantier' => $chantier,
             'competences' => $competences,
-            'ouvriers' => $ouvriers
-        ]);
-    }
-
-    #[Route(name: 'app_chantier_index', methods: ['GET'])]
-    public function index(ChantierRepository $chantierRepository): Response
-    {
-        return $this->render('chantier/index.html.twig', [
-            'chantiers' => $chantierRepository->findAll(),
+            'missions' => $missions
         ]);
     }
 
@@ -58,88 +49,23 @@ final class ChantierController extends AbstractController
         ]);
     }
 
-   
     #[Route('/{id}/edit', name: 'app_chantier_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Chantier $chantier, EntityManagerInterface $entityManager, CompetenceRepository $competenceRepository): Response
+    public function edit(Request $request, Chantier $chantier, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ChantierType::class, $chantier);
         $form->handleRequest($request);
 
-        $competences = $competenceRepository->findAll();
-
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
+
             return $this->redirectToRoute('app_admin_chantier', [], Response::HTTP_SEE_OTHER);
-        }
-
-        if ($request->isMethod('POST') && $request->request->get('form_type') === 'add_competence') {
-            $competenceId = $request->request->get('competence');
-
-            if (!empty($competenceId)) {
-                $competence = $competenceRepository->find($competenceId);
-
-                if ($competence) {
-                    $chantier->addCompetence($competence);
-                    $entityManager->flush();
-
-                    $this->addFlash('success', 'Compétence ajoutée au chantier avec succès !');
-                } else {
-                    $this->addFlash('error', 'Compétence introuvable.');
-                }
-            } else {
-                $this->addFlash('error', 'Veuillez sélectionner une compétence valide.');
-            }
-
-            return $this->redirectToRoute('app_admin_chantier', ['id' => $chantier->getId()]);
         }
 
         return $this->render('chantier/edit.html.twig', [
             'chantier' => $chantier,
-            'form' => $form->createView(),
-            'competences' => $competences,  
+            'form' => $form,
         ]);
     }
-
-
-
-    // #[Route('/{id}/edit', name: 'app_chantier_edit', methods: ['GET', 'POST'])]
-    // public function edit(Request $request, Chantier $chantier, EntityManagerInterface $entityManager, CompetenceRepository $competenceRepository): Response
-    // {
-    //     //insertion en bdd de la competence + chantier
-    //     if ($request->isMethod('POST')) {
-    //         $competenceId = $request->request->get('competence');
-    //         $competence = $competenceRepository->find($competenceId);
-
-    //         if ($competence) {
-    //             $chantier = new Chantier();
-    //             $chantier->addCompetence($competence);
-    //             $entityManager->persist($chantier);
-    //             $entityManager->flush();
-
-    //             $this->addFlash('success', 'Compétence ajoutée au chantier avec succès !');
-    //         } else {
-    //             $this->addFlash('error', 'Compétence introuvable.');
-    //         }
-    //         return $this->redirectToRoute('show_chantier', ['id' => $id]);
-    //     }
-
-    //     $competences = $competenceRepository->findAllCompetences();
-
-    //     $form = $this->createForm(ChantierType::class, $chantier);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $entityManager->flush();
-
-    //         return $this->redirectToRoute('app_admin_chantier', [], Response::HTTP_SEE_OTHER);
-    //     }
-
-    //     return $this->render('chantier/edit.html.twig', [
-    //         'chantier' => $chantier,
-    //         'form' => $form,
-    //         'competences' => $competences
-    //     ]);
-    // }
 
     #[Route('/{id}', name: 'app_chantier_delete', methods: ['POST'])]
     public function delete(Request $request, Chantier $chantier, EntityManagerInterface $entityManager): Response
